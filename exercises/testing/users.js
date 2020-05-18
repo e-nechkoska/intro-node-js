75da680bb0192f5f272edc7b2bfd359c7c51bec9
e-nechkoska <e.nechkoska@gmail.com>
2020-05-18 22:14:49 +0200

    solved test exercise

--- exercises/testing/users.js
+++ exercises/testing/users.js
@@ -7,29 +7,33 @@ const users = new Array(20).fill(0)
   }
 })
 
+const fixId = (id) => parseInt(id);
+
 // simulate async db call with promise
 const findUser = (id) => new Promise((resolve, reject) => {
-  const user = users.find(user => user.id === id)
+  const __id = fixId(id);
+  const user = users.find(user => user.id === __id)
   if (user) {
     return resolve(user)
   }
-  reject(new Error(`No user with id "${id}"`))
+  reject(new Error(`No user with id "${__id}"`))
 })
 
 // simulate async db call with promise
 const deleteUser = (id) => new Promise((resolve, reject) => {
-  const id = fixId(id)
-  const i = users.findIndex(user => user.id === id)
+  const __id = fixId(id)
+  const i = users.findIndex(user => user.id === __id)
 
   if (i < 0) {
-    return reject(new Error(`No user with id "${id}"`))
+    return reject(new Error(`No user with id "${__id}"`))
   }
 
   users.slice(i, 1)
-  resolve({id})
+  resolve({__id})
 })
 
 module.exports = {
   findUser,
-  deleteUser
+  deleteUser,
+  fixId
 }
